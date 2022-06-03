2adec0954696b3d0b08f0761f7d1de6e45c38833
Darlon Henrique <darlonhenrisouza@gmail.com>
2022-06-03 12:18:19 -0300

    refactory(Dashboard): change JSX for TSX and type it

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -6,19 +6,25 @@ import Food from '../../components/Food'
 import ModalAddFood from '../../components/ModalAddFood'
 import ModalEditFood from '../../components/ModalEditFood'
 import { FoodsContainer } from './styles'
+import { FoodInterface } from '../../types'
 
 function Dashboard() {
-  const [foods, setFoods] = useState([])
-  const [editingFood, setEditingFood] = useState({})
+  const [foods, setFoods] = useState<FoodInterface[]>([])
+  const [editingFood, setEditingFood] = useState<FoodInterface>(
+    {} as FoodInterface
+  )
   const [modalOpen, setModalOpen] = useState(false)
   const [editModalOpen, setEditModalOpen] = useState(false)
 
-  useEffect(async () => {
-    const response = await api.get('/foods')
-    setFoods(response.data)
+  useEffect(() => {
+    async function loadFoods() {
+      const response = await api.get('/foods')
+      setFoods(response.data)
+    }
+    loadFoods()
   })
 
-  const handleAddFood = async food => {
+  const handleAddFood = async (food: FoodInterface) => {
     try {
       const response = await api.post('/foods', {
         ...food,
@@ -30,7 +36,7 @@ function Dashboard() {
     }
   }
 
-  const handleUpdateFood = async food => {
+  const handleUpdateFood = async (food: FoodInterface) => {
     try {
       const foodUpdated = await api.put(`/foods/${editingFood.id}`, {
         ...editingFood,
@@ -47,7 +53,7 @@ function Dashboard() {
     }
   }
 
-  const handleDeleteFood = async id => {
+  const handleDeleteFood = async (id: number) => {
     await api.delete(`/foods/${id}`)
 
     const foodsFiltered = foods.filter(food => food.id !== id)
@@ -63,7 +69,7 @@ function Dashboard() {
     setEditModalOpen(!editModalOpen)
   }
 
-  const handleEditFood = food => {
+  const handleEditFood = (food: FoodInterface) => {
     setEditingFood(food)
     setEditModalOpen(true)
   }
